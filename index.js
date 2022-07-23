1f095bb3a21f569e82f2973a8dedfea9b3b03909
Chris Luong <christopher_luong@live.ca>
2022-07-23 14:35:37 -0400

    update getWeather to default search toronto if nothing is provided

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const APIKEY = '71423050a90fbe8000f73aa8b4fbe098'
 
-const  getWeather = async (location = "Toronto") => {
-    let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=${APIKEY}`, {mode: 'cors'})
-    let data = await response.json();
+const  getWeather = async () => {
+    try {
+        let form = document.getElementById('locationForm')
+        let location = form.elements['location'].value ? form.elements['location'].value : "Toronto"
 
-    let container = document.querySelector('.container');
-    container.innerHTML = JSON.stringify(data)
+        let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=${APIKEY}`, {mode: 'cors'})
+        let data = await response.json();
+
+        let processedData = processData(data)
+        displayWeather(processedData);
+        
+    } catch (error) {
+        console.log(error)
+    }
+    
+    
 } 
 
-const processData = () => {
+function displayWeather(data) {
+    let container = document.querySelector('.container');
+        container.innerHTML = JSON.stringify(data)
+}
+
+const processData = (data) => {
+    let temps = data.main
+    let weather = data.weather[0]
+
+    let processedData =  {
+        temps,
+        weather
+    }
+    return processedData
 
 }
 
-// getWeather();
+getWeather()
